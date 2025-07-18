83a94ac0fa84289d3be60968ef7defb1b2560bd6
utamuratov <43562558+utamuratov@users.noreply.github.com>
2025-07-18 16:11:15 +0500

    .

--- dist/kasb-top/server/angular-app-manifest.mjs
+++ dist/kasb-top/server/angular-app-manifest.mjs
@@ -33,8 +33,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 10962, hash: 'f72a79ff8b5f72b7703d7feef8581b2edbe01f48c1b612118c4d0aac6dda6fca', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 3107, hash: 'bd9da294825ef74ad50921a53e5b558e927a7f0368b716da14c115f1c121459d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 11030, hash: '86eb006c2a17819e504a8c5de843d4e35df9a83e69d221901e86428ae549923a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 3176, hash: '4a7bf7d28b1921a3595481680f8c4f808ccbfdbb9ef527bd33410b19d7191cc2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-GJDU2Y35.css': {size: 26879, hash: 'yQ6V0ZAbSZM', text: () => import('./assets-chunks/styles-GJDU2Y35_css.mjs').then(m => m.default)}
   },
 };
