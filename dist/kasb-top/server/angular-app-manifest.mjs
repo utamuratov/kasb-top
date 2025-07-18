aa4adfba405d174a153c115c796cbf5b14283df8
utamuratov <43562558+utamuratov@users.noreply.github.com>
2025-07-18 16:20:00 +0500

    .

--- dist/kasb-top/server/angular-app-manifest.mjs
+++ dist/kasb-top/server/angular-app-manifest.mjs
@@ -8,15 +8,15 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-22EUWMC4.js"
+      "chunk-XPKMMV7R.js"
     ],
     "route": "/"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-EDVHWGOF.js",
-      "chunk-ZFC7HBYA.js",
+      "chunk-WLRPAT73.js",
+      "chunk-YXV7UAZK.js",
       "chunk-MAGCBL5F.js"
     ],
     "route": "/test"
@@ -24,8 +24,8 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-CZBBKF5P.js",
-      "chunk-ZFC7HBYA.js",
+      "chunk-R5EWMOA6.js",
+      "chunk-YXV7UAZK.js",
       "chunk-MAGCBL5F.js"
     ],
     "route": "/result"
@@ -33,11 +33,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 11030, hash: '86eb006c2a17819e504a8c5de843d4e35df9a83e69d221901e86428ae549923a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 3176, hash: '4a7bf7d28b1921a3595481680f8c4f808ccbfdbb9ef527bd33410b19d7191cc2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 52015, hash: 'f8ded96e68e9d3d4e5780c013b2097acee8cfc151dc92daaf2c315a20643d062', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'test/index.html': {size: 39773, hash: '8dfb7e10542ac859b0569dc4ce1d493fd88974cfda038b4eab9860f4bac7abad', text: () => import('./assets-chunks/test_index_html.mjs').then(m => m.default)},
-    'result/index.html': {size: 94350, hash: '7f4c184c94d107a7c4c926e314feba09ab46fdab9a33ed1f1682d5dd0356191a', text: () => import('./assets-chunks/result_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 11030, hash: '383b938ad0b38969449c2eac1de8d6920d1a6603a7cc7bc9c6002bf783feb96f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 3176, hash: 'adf01b376e5fed76987d04d527016a58596a7311a9e9d11cfc5e74722ca567db', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 52015, hash: 'e18dabcfe51bfe7a3afe121ed485ae37556e50f5b75adc6cb9faa6771e039378', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'test/index.html': {size: 39773, hash: '0c6253e6dccf15e5e552a4e61b3aecd134d0ca5b648a9650a1c87bad83ed66b0', text: () => import('./assets-chunks/test_index_html.mjs').then(m => m.default)},
+    'result/index.html': {size: 94350, hash: '7859d07a22b559e2a8765fe77764c9bcf32f3a7f9a606766ab142ae582f7e972', text: () => import('./assets-chunks/result_index_html.mjs').then(m => m.default)},
     'styles-GJDU2Y35.css': {size: 26879, hash: 'yQ6V0ZAbSZM', text: () => import('./assets-chunks/styles-GJDU2Y35_css.mjs').then(m => m.default)}
   },
 };
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,45 +3,45 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {
-  provideClientHydration,
-  withEventReplay,
+    provideClientHydration,
+    withEventReplay,
 } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { definePreset } from '@primeng/themes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 const MyPreset = definePreset(Aura, {
-  semantic: {
-    primary: {
-      50: '{blue.50}',
-      100: '{blue.100}',
-      200: '{blue.200}',
-      300: '{blue.300}',
-      400: '{blue.400}',
-      500: '{blue.500}',
-      600: '{blue.600}',
-      700: '{blue.700}',
-      800: '{blue.800}',
-      900: '{blue.900}',
-      950: '{blue.950}',
+    semantic: {
+        primary: {
+            50: '{blue.50}',
+            100: '{blue.100}',
+            200: '{blue.200}',
+            300: '{blue.300}',
+            400: '{blue.400}',
+            500: '{blue.500}',
+            600: '{blue.600}',
+            700: '{blue.700}',
+            800: '{blue.800}',
+            900: '{blue.900}',
+            950: '{blue.950}',
+        },
     },
-  },
 });
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(),
-    provideRouter(routes),
-    provideClientHydration(withEventReplay()),
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        // preset: Aura,
-        preset: MyPreset,
-      },
-    }),
-  ],
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideHttpClient(withFetch()),
+        provideRouter(routes),
+        provideClientHydration(withEventReplay()),
+        provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                // preset: Aura,
+                preset: MyPreset,
+            },
+        }),
+    ],
 };
